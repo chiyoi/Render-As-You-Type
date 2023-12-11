c27b7fb25a54f918c5302db23e7abfbfd46c123c
chiyoi <chiyoi2140@outlook.com>
2023-12-11 22:44:44 +0800

    init

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { useState } from 'react'
+import 'katex/dist/katex.min.css'
+import Latex from 'react-latex-next'
+import { Flex, Heading, TextArea } from '@radix-ui/themes'
+
+export default function Page() {
+  const [formula, setFormula] = useState('')
+  return (
+    <Flex mx='auto' mb='auto' gap='2' direction='column' style={{ minWidth: '40vw' }}>
+      <Heading align='center'>LaTeX</Heading>
+      <TextArea placeholder='Input formula...' value={formula} onChange={e => setFormula(e.target.value)} />
+      <Latex>{'$$' + formula + '$$'}</Latex>
+    </Flex>
+  )
+}
