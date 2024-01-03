568077e6e6f3b6cf7f6487691499ccffe14afe9b
chiyoi <chiyoi2140@outlook.com>
2024-01-03 17:05:51 +0800

    tidy

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export const viewport: Viewport = {
   ],
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <body style={{ margin: 0 }}>
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import Latex from 'react-latex-next'
 import { Flex, Heading, TextArea } from '@radix-ui/themes'
 
-export default function Page() {
+export default () => {
   const [formula, setFormula] = useState('')
   return (
     <Flex mx='auto' align='center' mb='auto' gap='3' direction='column'>
