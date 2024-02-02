de0c7c03c58ff2ca2acbb27c4f6f208ccf0fb81f
chiyoi <chiyoi2140@outlook.com>
2024-02-03 02:20:12 +0800

    Refactor page.tsx to use custom font and improve code formatting

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,19 @@
 import { useState } from 'react'
 import Latex from 'react-latex-next'
 import { Flex, Heading, TextArea } from '@radix-ui/themes'
+import { FontNotoSansMono } from '@/app/internal/fonts'
 
 export default () => {
   const [formula, setFormula] = useState('')
   return (
     <Flex mt='9' mx='auto' align='center' mb='auto' gap='3' direction='column'>
-      <Heading align='center'>LaTeX</Heading>
-      <TextArea size='3' placeholder='Input formula...' value={formula} onChange={e => setFormula(e.target.value)} style={{ width: '50vw' }} />
+      <Heading align='center' style={{ ...FontNotoSansMono }}>LaTeX</Heading>
+      <TextArea size='3' placeholder='Input formula...' value={formula} onChange={
+        e => setFormula(e.target.value)
+      } style={{
+        ...FontNotoSansMono,
+        width: '50vw',
+      }} />
       <Latex>{formula}</Latex>
     </Flex>
   )
