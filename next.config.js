f7c83f80ae3a45611b900f8edadeb543dfebc6ec
chiyoi <chiyoi2140@outlook.com>
2024-03-13 16:15:04 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -8,15 +8,6 @@ module.exports = (phase, { defaultConfig }) => ({
 
 /** @type {import('next').NextConfig} */
 const commonConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'opengraph.githubassets.com',
-        pathname: '/5646866ce296c2c5516afb70c27af222ca848b52e20b39d0f652a107c9556dd6/**',
-      },
-    ],
-  },
 }
 
 /** @type {import('next').NextConfig} */
